3b80e157a3b9d708b0145f421d8f72b681be1a20
Alexander May <alexander.may@hey.com>
2023-10-25 20:38:27 +0200

    i finished setting up

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,118 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+
+import { SimpleLayout } from '@/components/SimpleLayout'
+import googleCyberSecurityLogo from '@/images/logos/google_cybersecurity.png'
+
+function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Skills',
+  description: "Skills and Technologies I've used",
+}
+
+const skills = [
+  {
+    label: 'Frontend',
+    skills: [
+      'Angular',
+      'AngularJS',
+      'React.js',
+      'Next.js',
+      'CSS',
+      'SCSS',
+      'HTML',
+      'Tailwind',
+      'Angular Material',
+      'Typescript',
+      'Javascript',
+      'NgRx',
+      'RxJS',
+      'Jest',
+      'Testing-Library',
+      'Cypress',
+      'Jasmine',
+      'Protractor',
+      'Selenium',
+      'Webpack',
+      'Ionic',
+      'Redux',
+    ],
+    colors:
+      'bg-purple-50 text-purple-700 ring-purple-700/10 dark:bg-purple-400/10 dark:text-purple-400 dark:ring-purple-400/30',
+  },
+  {
+    label: 'Backend',
+    skills: ['Node.js', 'C++', 'C#', '.NET', 'EF', 'Python', 'Java', 'SQL'],
+    colors:
+      'bg-blue-50 text-blue-700 ring-blue-700/10 dark:bg-blue-400/10 dark:text-blue-400 dark:ring-blue-400/30',
+  },
+  {
+    label: 'Tools, Platforms & Other',
+    skills: [
+      'Git',
+      'GitLab',
+      'GitHub',
+      'Github Actions',
+      'Circle CI',
+      'Jenkins',
+      'Bitbucket',
+      'SVN',
+      'AWS',
+      'Unity',
+      'MacOS',
+      'Windows',
+      'Linux',
+      'Bash',
+      'Powershell',
+      'Jira',
+      'Confluence',
+      'Keycloak',
+      'OAuth',
+      'Scrum',
+      'Agile',
+    ],
+    colors:
+      'bg-red-50 text-red-700 ring-red-700/10 dark:bg-red-400/10 dark:text-red-400 dark:ring-red-400/30',
+  },
+]
+export default function Projects() {
+  return (
+    <SimpleLayout
+      title="Skills and Technologies"
+      intro="These are the different skills, technologies & co I've used over the years. I'm not equally skilled in all of them, but we could probably have a conversation about each of them. Not necessarily to show off, I just thought it's cool to see the amount of knowledge you built up in one place!"
+    >
+      {skills.map((category) => (
+        <div key={category.label} className="mb-6">
+          <h2 className="mb-2 text-xl font-medium">{category.label}</h2>
+          <ul role="list" className="gap flex flex-wrap gap-2">
+            {category.skills.map((skill) => (
+              <span
+                key={skill}
+                className={`inline-flex items-center rounded-full px-3 py-1 text-xxs font-medium ring-1 ring-inset ${category.colors}`}
+              >
+                {skill}
+              </span>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <h2 className="mb-2 text-xl font-medium">Certificates</h2>
+      <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6">
+        <Image
+          src={googleCyberSecurityLogo}
+          alt="Google Cybersecurity Certificate"
+        />
+      </ul>
+    </SimpleLayout>
+  )
+}
---
+++ src/app/uses/page.tsx
@@ -0,0 +1,106 @@
+import { Card } from '@/components/Card'
+import { ISection, Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+type AddField<T, K extends string, V> = T & { [P in K]: V }
+
+function ToolsSection({
+  children,
+  ...props
+}: AddField<ISection, 'space', string | undefined>) {
+  return (
+    <Section {...props}>
+      <ul role="list" className={props.space}>
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({
+  title,
+  href,
+  children,
+}: {
+  title: string
+  href?: string
+  children?: React.ReactNode
+}) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      {children ? <Card.Description>{children}</Card.Description> : null}
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Software I use, gadgets I love, and other things I recommend.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Software I use, gadgets I love, and other things I recommend."
+      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Workstation" space="space-y-8">
+          <Tool title="14” MacBook Pro, M2 Pro, 64GB RAM (2023)">
+            I just love the M chip, it’s so fast for development workloads and
+            stays silent pretty much all the time.
+          </Tool>
+          <Tool title="Logitech MX Keys Mini">
+            I tried quite a few keyboards for work, but never really found one I
+            liked. This one is small, connects quick, has good battery, typing
+            feels good and it’s silent so I don’t annoy my coworkers when in the
+            office.
+          </Tool>
+          <Tool title="Logitech MX Master 3">
+            Well.. It’s a mouse. Good battery, connects quickly. Does mouse
+            things. Recommend!
+          </Tool>
+          <Tool title="Herman Miller Aeron Chair">
+            If I’m going to slouch in the worst ergonomic position imaginable
+            all day, I might as well do it in an expensive chair.
+          </Tool>
+          <Tool title="Fully Jarvis Standing Desk">
+            I think they’ve been bought my HermanMiller, so maybe it’s a
+            HermanMiller Jarvis now? Anyway - it’s got programmable heights and
+            is pretty stable for the price. I got the 180x80 version, which is
+            enough space for my desktop pc, two monitors, and my macbook.
+          </Tool>
+          <Tool title="Secretlab Titan">
+            It’s a nice chair, the quality is better than its
+            &ldquo;gamer&rdquo; looks would suggest.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Development tools" space="space-y-8">
+          <Tool title="VS Code" />
+          <Tool title="Warp">
+            A modern take on terminals built on rust. It’s essentially a
+            terminal but with QOL features I always wished for - properly
+            selecting things, copy/paste, easily save commands
+          </Tool>
+          <Tool title="Raycast">
+            Probably the tool I miss the most on my Windows machine.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Desktop" space="space-y-4">
+          <Tool title="Fractal Design North" />
+          <Tool title="AMD Ryzen 5800X" />
+          <Tool title="Thermalright Peerless Assassin 120 SE" />
+          <Tool title="XFX Speedster MERC Radeon RX 7900 XT" />
+          <Tool title="Gigabyte X570 Aorus Elite" />
+          <Tool title="4x Corsair Vengeance RGB 8GB" />
+          <Tool title="Samsung 990 Pro 2TB" />
+          <Tool title="Crucial MX500 2TB" />
+          <Tool title="Corsair RM850x" />
+        </ToolsSection>
+      </div>
+    </SimpleLayout>
+  )
+}
